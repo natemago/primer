7e056a38e44e4de69da6ff6edacafa12dbdcf199
Pavle Jonoski <pavle@lugola.net>
2012-05-03 19:34:10 +0200

    VGA Compatibile text mode

---
+++ risc-emu/hw/vga.js
@@ -0,0 +1,16 @@
+(function($){
+   /*
+    * VGA, SVGA, QVGA
+    */
+    
+    
+    // Text mode...
+    // http://en.wikipedia.org/wiki/VGA-compatible_text_mode
+   var VGA = function(config){
+      libDraw.ext(this, config);
+   };
+   libDraw.ext(VGA,{
+        
+   });
+    
+})(jQuery);
