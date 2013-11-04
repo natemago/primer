896759fa82fa48852eeca5601fc62ab6343afdd1
Pavle Jonoski <pavle@lugola.net>
2013-11-05 00:41:46 +0100

    Added development profile glue file. This file specifies the binding of the libraries dependencies (such as jQuery, libDraw, etc)

---
+++ profiles/development.glue.js
@@ -0,0 +1,13 @@
+(function(){
+    /**
+     * Define libraries used acorss the app
+     */
+     
+     // jQuery
+     def('$', [], function(){return jQuery;});
+     
+     // libdraw
+     def('libDraw', [], function(){ return libDraw;});
+     
+    
+})();
