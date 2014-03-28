811fc9642d76edd5cde70b0e955edc62cac534a4
Pavle Jonoski <pavle@lugola.net>
2014-03-28 12:03:06 +0100

    Implemented the Macro class.

--- src/core/macro-processor.js
+++ src/core/macro-processor.js
@@ -15,7 +15,12 @@ def('risc.core.MacroProcessor',
     libDraw.ext(MacroProcessor, {
         process: function(fn, macroName, context, arguments){
             var md = this.buildMacroDef(fn, macroName);
+            libDraw.each(context, function(macro, macroName){
+                md.body = macro.expand(md.body);
+            });
             
+            var macro =  new Macro(md);
+            return macro;
         },
         buildMacroDef: function(fn, name){
             var RE_MACRO_ARGS = /^function\s+\(([^\)]+)\)\s?{/m;
@@ -44,10 +49,73 @@ def('risc.core.MacroProcessor',
     
     var Macro = function(config){
         libDraw.ext(this, config);
+        this.compile();
     };
     
     libDraw.ext(Macro, {
-        
+        RE_MACRO_ARG: '\\s?([\\w\\d\\.\\$\\[\\]_]+)',
+        compile: function(){
+            if(this.compiled){
+                return;
+            }
+            this.compiled = true;
+            this.buildRE();
+        },
+        _buildRE: function(){
+            var reStr = [this.name, '\\s?\\('];
+            reStr.push(this.arguments.map(function(arg){
+                return Macro.RE_MACRO_ARG;
+            }).join(','));
+            reStr.push('\\s\\)');
+            this.regExpString = reStr.join('');
+            this.argsRegExps = {};
+            libDraw.each(this.arguments, function(argName){
+                this.argsRegExps[argName] = '\\b'+argName+'\\b';
+            });
+        },
+        getRegRxp: function(modifiers){
+            return new RegExp(this.regExpString, modifiers || 'mg');
+        },
+        expand: function(str){
+            var expanded = str;
+            var mre = this.getRegExp();
+            var match, matched = {};
+            
+            while( (match = mre.exec(str) )!==null ){
+                expanded = expanded.replace(match[0], this.expandArgs(match[0]));
+            }
+            
+            return expanded;
+        },
+        expandArgs: function(match){
+            // get the actual arguments first:
+            var str = match.substring(match.indexOf('(') + 1, 
+                match.lastIndexOf(')') );
+            // map arg name -> replacement
+            var argRe = new RegExp(Macro.RE_MACRO_ARG, 'gm');
+            var ma, argMapping = {};
+            var  i = 0;
+            while( (ma = argRe.exec(str)) !== null ){
+                if( this.arguments[i] === undefined ){
+                    throw new Error('Too many arguments in macro: ' + 
+                        this.name + '. Expression: ' + match);
+                }
+                argMapping[this.arguments[i]] = ma[1];
+                i++;
+            }
+            if(this.arguments.length > i){
+                throw new Error('Too few arguments in macro: ' + 
+                        this.name + '. Expression: ' + match);
+            }
+            // replace each argument with the replacement in the macro body
+            var expandedMacro = this.body;
+            libDraw.each(this.arguments, function(argName){
+                expandedMacro = expandedMacro.replace(
+                    new RegExp(this.argsRegExps[argName], 'gm'), argMapping[argName]);
+            }, this);
+            
+            return expandedMacro;
+        }
     });
     
     return MacroProcessor;
