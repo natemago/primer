aea181e4fe56c6ba582b287ca3d8021a2bb83f2a
Pavle Jonoski <pavle@lugola.net>
2014-03-31 02:46:49 +0200

    Basic interfaces for the unit test mechanism:  - TestRunner  - TestSuite  - TestCase  - Report

--- src/core/unit-test.js
+++ src/core/unit-test.js
@@ -34,9 +34,68 @@ function($, libDraw, writer){
         libDraw.ext(Logger, ex);
     });
     
-    // Test Suite
+    var TestRunner = function(config){
+        libDraw.ext(this, config);
+        this.suites = {
+            inOrder: [],
+            all: {}
+        };
+        
+    };
     
-    // Test Case 
+    libDraw.ext(TestRunner, {
+        addSuite: function(suiteName, description, suite, testSetup, testTeardown){
+        
+        },
+        getSuite: function(suiteName){
+        },
+        runSuite: function(suiteName){},
+        runAll: function(){}
+    });
     
-    // Asserts
+    var TestSuite = function(config){
+        libDraw.ext(this, config);    
+    };
+    
+    libDraw.ext(TestSuite, {
+        addTestCase: function(testCaseName, description, testCase){},
+        getTest: function(name){
+        
+        },
+        runTest: function(testCaseName){
+        
+        }
+    });
+    
+    var TestCase = function(config){
+        libDraw.ext(this, config);
+    };
+    
+    libDraw.ext(TestCase, {
+        run: function(){
+            var self = this;
+            var assert = function( value, errorMessage) {
+                if(!value){
+                    self.reportError(errorMessage);
+                    throw new Error('Assert failed: ' + errorMessage);
+                }
+            };
+            try{
+                this.testCase.call(this.testContext, assert, this.logger);
+            }catch(e){
+                
+            }
+        },
+        reportError: function(errorMsg){
+            
+        }
+    });
+    
+    var Report = function(){};
+    
+    libDraw.ext(Report, {
+        testSuccess: function(testName){},
+        testFailed: function(testName, message){}
+        getStatistics: function(){}
+    });
 });
