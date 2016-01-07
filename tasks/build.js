c544ec24462a54989b021f6fa6b40fe305f05439
Pavle Jonoski <jonoski.pavle@gmail.com>
2016-01-07 16:37:49 +0100

    Serving from .serve-root

--- tasks/build.js
+++ tasks/build.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
+var symlink = require('gulp-symlink');
 
 gulp.task('build:web', ['inject:web'], function(){
 
 });
 
 gulp.task('build:dev', ['clean:build', 'build:web'], function(){
-
+  gulp.src('./build/*')
+    .pipe(gulp.dest('./.serve-root'));
+  gulp.src('./bower_components')
+    .pipe(symlink('./.serve-root/bower_components'));
 });
--- tasks/serve.js
+++ tasks/serve.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var serve = require('gulp-serve');
 
-gulp.task('serve', serve({
-  root: ['build', '.'],
+gulp.task('serve', ['build:dev'], serve({
+  root: ['./.serve-root', '.'],
   port: 4000,
   hostname: 'localhost'
 }));
