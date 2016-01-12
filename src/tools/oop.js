23cc8d54ea5125812276c53c6de47e992433cead
Pavle Jonoski <jonoski.pavle@gmail.com>
2016-01-12 19:53:47 +0100

    Added documentation to the OOP extend function.

--- src/tools/oop.js
+++ src/tools/oop.js
@@ -1,5 +1,4 @@
 (function(){
-  
   var merge = function(a, b){
     for(var propName in b){
       if(b.hasOwnProperty(propName)){
@@ -49,7 +48,119 @@
 
   def('oop', function(){
     return {
+      /**
+       * Performs a classical OOP class extend.
+       * When given a function as a first argument, it will be considered as a class
+       * and it will be extended with the properties of the next arguments.
+       * If there are no other functions given as arguments, then all other orguments
+       * are considered to be mixins. This case in general is as follows:
+       * <code>
+       * extend(SubClass:function, mixin:object [,mixin:object ...])
+       * </code>
+       * The properties (methods) of the mixins will be merged with the SubClass prototype
+       * evaulating from left to right, meaning any methods that are present in the class itself
+       * or in a mixin can be overwritten by another mixin to the right of it.
+       * For example:
+       * <code>
+       *  var SubClass = function(){};
+       *  extend(SubClass, {
+       *  	mixin1_method: function()
+       *  		// will be in the final class
+       *  	},
+       *  	method: function(){
+       *  	  // this will be overwritten by the next mixin
+       *  	}
+       *  },{
+       *   method: function(){
+       *     // this method will be available in the final class
+       *   }
+       *  })
+       * </code>
+       *
+       *  If other functions are supplied to the extend
+       *  function after the first class, then the first (left most after the first argument)
+       *  will be considered as the super class for extend. The general synopsis is:
+       *  <code>
+       *  var SubClass = function(){};
+       *  var SuperClass = function(){};
+       *
+       *  extend(SubClass, SuperClass);
+       *  console.log(SubClass.superclass == SuperClass.prototype); // returns true
+       *  var obj = new SubClass();
+       *  console.log(obj instanceof SubClass); // true
+       *  console.log(obj instanceof SuperClass); // true
+       *  </code>
+       *  Or let's consider the following, a bit more complex example:
+       *
+       * <code>
+       * var SuperClass = function(){}; // constructor for the super class
+       * extend(SuperClass, {
+       *   superClassMethod: function(){}
+       * });
+       *
+       * var SubClass = function(){}; // constructor for the sub class
+       *
+       * // we'll extend from the super class and add the methods for the sub class:
+       * extend(SubClass, SuperClass, {
+       *   subClassMethod: function(){} // this is the sub class method
+       * });
+       *
+       * var obj = new SubClass();
+       *
+       * console.log(obj.superClassMethod !== undefined); // true, inherited from super class
+       * console.log(obj.subClassMethod !== undefined); // true, from the sub class definition
+       * </code>
+       *
+       * When we extend a class with another class, a special property called <code>superclass</code> is
+       * attached to the class itself. This points to the super class prototype, so we can easily access the
+       * methods or properties available in the super class (or methods that we have overriden in the sub class).
+       * For example, here is how we can call a super method from a child class:
+       * <code>
+       * var A = function(){
+       *   console.log('A constructor call');
+       * };
+       *
+       * extend(A, {
+       *   method: function(value){
+       *     console.log('A.method() call with value: ', value)
+       *   }
+       * });
+       *
+       * var B = function(){
+       *   // call to the super constructor
+       *   B.superclass.constructor.call(this);
+       *   console.log('B constructor call')
+       * };
+       *
+       * extend(B, A, {
+       *   otherMethod: function(){
+       *     // a call to a super class mathod with value
+       *   	 B.superclass.method.call(this, 'some value');
+       *   	 console.log('B.otherMethod() call')
+       *   }
+       * });
+       *
+       * var b = new B(); // Console will log:
+       * // A constructor call
+       * // B constructor call
+       *
+       * b.otherMethod(); // will log:
+       * // A.method() call with value: some value
+       * // B.otherMethod() call
+       *
+       * </code>
+       * Note that when calling the super class methods, we do it by calling the
+       * special method <code>Function.call()</code>. We do this to set the funcion
+       * <code>this</code> to point to the proper object.
+       *
+       * Another usage of the <code>extend</code> function is to merge two or more
+       * objects. This happens if you provide the function with an object that is not
+       * a funcion as a first argument. In that case, no OOP class extend will be atempted.
+       */
       extend: extend,
+      /**
+       * An alias for extend
+       */
       ext: extend,
       merge: merge
     };
