151f7761bf6187d87ca0c191170715b7dfc80503
Pavle Jonoski <jonoski.pavle@gmail.com>
2016-01-13 19:21:48 +0100

    Lexer for text formatting

--- src/text/format.js
+++ src/text/format.js
@@ -1,6 +1,42 @@
 (function(){
-  var Parser = function(){};
+  def('text.format', ['oop'], function(oop){
 
-  // "{color:red}Test{/color}"
+    var Lexer = function(text){
+      this.text = text;
+      this.regex = /\{([^\{\}]+)\}/gm;
+      this.lastIndex = 0;
+      this.endOfText = false;
+    };
+
+    oop.extend(Lexer, {
+      nextToken: function(){
+        if(this.endOfText){
+          return null;
+        }
+        var res = this.regex.exec(this.text);
+        if(res){
+          var command = res[1];
+          var text = this.text.substring(this.lastIndex, this.regex.lastIndex-res[0].length);
+          this.lastIndex = this.regex.lastIndex;
+          return {
+            text: text,
+            command: command
+          };
+        }else{
+          this.endOfText = true;
+          if(this.lastIndex == this.text.length){
+            return null;
+          }
+          return {
+            text: this.text.substring(this.lastIndex, this.text.length)
+          };
+        }
+      }
+    });
+
+    return {
+      Lexer: Lexer
+    };
+  });
 
 })();
--- tests/src/text/format.spec.js
+++ tests/src/text/format.spec.js
@@ -1,7 +1,7 @@
 (function(){
-  def('text.format.tests',['unit:test:suite'], function(suite){
+  def('text.format.tests',['unit:test:suite', 'text.format'], function(suite, txtf){
     suite('Research tests','', function(usecase){
-      usecase('Research regex', '', function(ok, log, expect){
+      usecase('Research directive regex', '', function(ok, log, expect){
         var sampleFormattedText = 'Th{}is}{ is {color:red}red text{/color} { ssss{}s';
         var directiveRegex = /\{[^\{\}]+\}/gm;
         var res = directiveRegex.exec(sampleFormattedText);
@@ -13,6 +13,69 @@
         ok(res);
         ok(res.length);
         expect(res[0], '{/color}');
+      });
+
+    });
+
+    suite('Lexer', 'Text format lexer', function(usecase){
+      usecase('Parse tokens - single command, start of line', 'Parse tokens', function(ok, log, expect){
+        var txt = '{color:green} rest of line.';
+        var lexer = new txtf.Lexer(txt);
+
+        var token = lexer.nextToken();
+        ok(token);
+        ok(token.command);
+        ok(!token.text);
+        expect(token.command, 'color:green');
+
+        token = lexer.nextToken();
+        ok(token);
+        ok(!token.command);
+        ok(token.text);
+        expect(token.text, ' rest of line.');
+
+        token = lexer.nextToken();
+        ok(!token);
+        log.info('Token is none -> [', token, ']');
+      });
+
+      usecase('Parse tokens - single command, middle of line', 'Parse tokens', function(ok, log, expect){
+        var txt = 'start of line {color:green} rest of line.';
+        var lexer = new txtf.Lexer(txt);
+
+        var token = lexer.nextToken();
+        ok(token);
+        ok(token.command);
+        ok(token.text);
+        expect(token.command, 'color:green');
+        expect(token.text, 'start of line ');
+
+        token = lexer.nextToken();
+        ok(token);
+        ok(!token.command);
+        ok(token.text);
+        expect(token.text, ' rest of line.');
+
+        token = lexer.nextToken();
+        ok(!token);
+        log.info('Token is none -> [', token, ']');
+      });
+
+      usecase('Parse tokens - single command, end of line', 'Parse tokens', function(ok, log, expect){
+        var txt = 'start of line {color:green}';
+        var lexer = new txtf.Lexer(txt);
+
+
+        var token = lexer.nextToken();
+        ok(token);
+        ok(token.command);
+        ok(token.text);
+        expect(token.command, 'color:green');
+        expect(token.text, 'start of line ');
+
+        token = lexer.nextToken();
+        ok(!token);
+        log.info('Token is none -> [', token, ']');
       });
     });
   });
