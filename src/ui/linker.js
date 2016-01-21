b7b4e3f98e12b4cf391f526b5232197d42c56b11
Pavle Jonoski <jonoski.pavle@gmail.com>
2016-01-21 18:37:31 +0100

    Implemented Linker interfaces

--- src/ui/linker.js
+++ src/ui/linker.js
@@ -3,6 +3,27 @@
  */
 (function(){
   def('ui.linker', ['$', 'oop'], function($, oop){
-    
+
+    var Linked = function(config){
+      oop.ext(this, config);
+    };
+
+    oop.extend(Linked, {
+      link: function(){
+
+      }
+    });
+
+    var Linker = function(){};
+
+    oop.extend(Linker, {
+      link: function(el, context){
+
+      }
+    });
+
+    return {
+      Linker: Linker
+    };
   });
 })();
